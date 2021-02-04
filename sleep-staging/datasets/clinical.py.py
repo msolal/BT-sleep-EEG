2c5567e4600a875812c4f8cc5c250bac4ecc6368
olivierpallanca <olivierpallanca@gmail.com>
2021-02-04 16:13:27 +0100

    get raw from clinical data

--- sleep-staging/datasets/clinical.py.py
+++ sleep-staging/datasets/clinical.py.py
@@ -2,19 +2,21 @@ import os
 import mne
 import numpy as np
 import pandas as pd
+from mne_bids import BIDSPath, read_raw_bids
 
 from braindecode.datasets.base import BaseDataset, BaseConcatDataset
 
 
-class MASS_SS3(BaseConcatDataset):
+class ClinicalDataset(BaseConcatDataset):
     """MASS SS3 dataset.
     Contains overnight recordings from 62 healthy subjects.
 
     Parameters
     ----------
-    subject_ids: list(int) | None
-        list of int of subject(s) to be loaded. If None, load all available
-        subjects.
+    subject_ids: list(str) | int | None
+        list of str of subject(s) to be loaded. 
+        If None, load all available subjects.
+        If int, load first subject_ids subjects. 
     preload: bool
         If True, preload the data of the Raw objects.
     load_eeg_only: bool
@@ -28,31 +30,37 @@ class MASS_SS3(BaseConcatDataset):
     def __init__(self, subject_ids=None, preload=False,
                  load_eeg_only=True, crop_wake_mins=30):
 
-        not_available = {36, 40, 43, 45}
+        all_sub = pd.read_csv('BIDS/participants.tsv', delimiter='\t',
+                              skiprows=1,
+                              names=['participant_id', 'age', 'sex', 'hand'],
+                              engine='python')['participant_id'].transform(
+                                  lambda x: x[4:]).tolist()
+
         if subject_ids is None:
-            subject_ids = [x for x in range(1, 47) if x not in not_available]
-        if len(set(subject_ids).intersection(not_available)) != 0:
-            subject_ids = subject_ids - not_available
-            print('Warning: subjects 36, 40, 43 and 45 are not available.')
+            subject_ids = all_sub
+        elif type(subject_ids) == int:
+            subject_ids = all_sub[:subject_ids]
+        elif len(set(subject_ids).intersection(all_sub)) != len(subject_ids):
+            subject_ids = [x for x in subject_ids if x in all_sub]
+            print('Warning: selected subject which doesn\'t exist')
 
-        rec_nb = ['01-03-000{}'.format(i) if i < 10 else '01-03-00{}'.format(i)
-                  for i in subject_ids]
-
-        paths = [(f'/storage/store/data/mass/SS3/{i} PSG.edf',
-                  f'/storage/store/data/mass/SS3/annotations/{i} Annotations.edf')
-                 for i in rec_nb]
+        bids_root = '/media/pallanca/datapartition/maelys/BIDS'
+        if load_eeg_only:
+            datatype = 'eeg'
+        bids_paths = [BIDSPath(subject=subject, datatype=datatype,
+                               root=bids_root) for subject in subject_ids]
 
         all_base_ds = list()
-        for p in paths:
-            raw, desc = self._load_raw(
-                p[0], p[1], preload=preload, load_eeg_only=load_eeg_only,
-                crop_wake_mins=crop_wake_mins)
+        for path in bids_paths:
+            raw, desc = self._load_raw(path, preload=preload,
+                                       load_eeg_only=load_eeg_only,
+                                       crop_wake_mins=crop_wake_mins)
             base_ds = BaseDataset(raw, desc)
             all_base_ds.append(base_ds)
         super().__init__(all_base_ds)
 
     @staticmethod
-    def _load_raw(raw_fname, annot_fname, preload, load_eeg_only=True,
+    def _load_raw(bids_path, preload, load_eeg_only=True,
                   crop_wake_mins=False):
         ch_mapping = {
             'ECG I': 'ecg',
@@ -64,14 +72,16 @@ class MASS_SS3(BaseConcatDataset):
         }
         exclude = ch_mapping.keys() if load_eeg_only else ()
 
-        raw = mne.io.read_raw_edf(raw_fname, preload=preload, exclude=exclude)
-        annots = mne.read_annotations(annot_fname)
-        raw.set_annotations(annots, emit_warning=False)
+        raw = read_raw_bids(bids_path=bids_path,
+                            extra_params=dict(exclude=exclude),
+                            verbose=False)
+        annots = raw.annotations
 
         if crop_wake_mins > 0:
             # Find first and last sleep stages
             mask = [
-                x[-1] in ['1', '2', '3', '4', 'R'] for x in annots.description]
+                x[-1] in ['1', '2', '3', '4', 'R'] 
+                for x in annots.description]
             sleep_event_inds = np.where(mask)[0]
 
             # Crop raw
@@ -89,9 +99,9 @@ class MASS_SS3(BaseConcatDataset):
         if not load_eeg_only:
             raw.set_channel_types(ch_mapping)
 
-        basename = os.path.basename(raw_fname)
-        subj_nb = basename[:10]
-        sess_nb = basename[:10]
-        desc = pd.Series({'subject': subj_nb, 'recording': sess_nb}, name='')
+        basename = bids_path.basename
+        sub_nb = basename[4:8]
+        ses_nb = basename[13:19]
+        desc = pd.Series({'subject': sub_nb, 'recording': ses_nb}, name='')
 
         return raw, desc
