237426b18f72e5efe8795878cfb45ca3a9aa1857
MaelysSolal <maelys.solal@polytechnique.edu>
2021-03-13 18:50:45 +0100

    save and load pre-trained models

--- sleep-staging/main_load.py
+++ sleep-staging/main_load.py
@@ -5,18 +5,11 @@ import numpy as np
 import os
 import torch
 
-from braindecode.datasets import BaseConcatDataset
 from datasets.bids import BIDS
 from braindecode.datautil.preprocess import (zscore, NumpyPreproc,
                                              MNEPreproc, preprocess)
 from braindecode.datautil.windowers import create_windows_from_events
-from datautil.split import split_by_events
 from visualisation.windows import view_nb_windows
-from util import set_random_seeds
-from braindecode.models.sleep_stager_chambon_2018 import SleepStagerChambon2018
-from skorch.helper import predefined_split
-from skorch.callbacks import EpochScoring
-from braindecode import EEGClassifier
 from sklearn.metrics import (confusion_matrix, classification_report,
                              balanced_accuracy_score, cohen_kappa_score)
 from visualisation.results import (save_score, plot_confusion_matrix,
@@ -40,12 +33,6 @@ classes_mapping = {'0': 'W', '1': 'N1', '2': 'N2', '3': 'N3', '4': 'R'}
 # # datasets = ['MASS_SS3', 'SleepPhysionet']
 # # derivatives = ['2channels', 'preprocessed']
 # sizes = [48, 12]
-# datasets = ['MASS_SS3']
-# derivatives = ['2channels']
-datasets = ['Clinical']
-derivatives = ['preprocessed']
-sizes = [60]
-train_test_diff = True if len(datasets) > 1 else False
 
 sfreq = 100
 window_size_s = 30
@@ -53,12 +40,12 @@ lr = 5e-4           # lr can be 5e-4 or 1e-3
 n_epochs = 10
 batch_size = 8
 
-print_datasets = (f'{datasets[0]}_{datasets[1]}'
-                  if train_test_diff else datasets[0])
-print_sizes = (f'{sizes[0]}_{sizes[1]}' if train_test_diff
-               else str(sizes[0]))
+print_datasets = f'{datasets[0]}_{datasets[1]}'
+print_sizes = f'{sizes[0]}_{sizes[1]}'
 
-plots_path = f'plots/clean_microV/{print_datasets}_preproc-{print_sizes}-lr{lr}_batch{batch_size}_{n_epochs}epochs/'
+plots_path = f'plots/{print_datasets}-{print_sizes}-lr{lr}_batch{batch_size}_{n_epochs}epochs/'
+train_desc = f'{datasets[0]}-{sizes[0]}-lr{lr}_batch{batch_size}_{n_epochs}epochs/'
+models_path = '/storage/store2/work/msolal/trained_models/' + train_desc
 
 # %%
 # 1. Loading the data
@@ -69,10 +56,9 @@ try:
 except FileExistsError:
     print(f'Directory {plots_path} already exists\n')
 
-dataset = BaseConcatDataset([BIDS(dataset=datasets[i],
-                                  derivatives=derivatives[i],
-                                  subject_ids=sizes[i])
-                            for i in range(len(datasets))])
+dataset = BIDS(dataset=datasets[1],
+               derivatives=derivatives[1],
+               subject_ids=sizes[1]))
 print(dataset.description)
 
 # %%
@@ -92,60 +78,16 @@ preprocess(windows_dataset, [MNEPreproc(fn=zscore)])
 # %%
 # 3. Making train, valid and test splits
 
-train_set, valid_set, test_set = split_by_events(windows_dataset, datasets)
-print(view_nb_windows(plots_path, train_set, valid_set, test_set))
+test_set = windows_dataset
+print(view_nb_windows(plots_path, None, None, test_set))
 
 # %%
-# 4. Creating the model
+# 4. Loading the model
 
-cuda = torch.cuda.is_available()
-set_random_seeds(seed=87, cuda=cuda)
-device = 'cuda' if cuda else 'cpu'
-if cuda:
-    torch.backends.cudnn.benchmark = True
-
-n_classes = 5
-n_channels = train_set[0][0].shape[0]
-input_size_samples = train_set[0][0].shape[1]
-
-# Create model
-model = SleepStagerChambon2018(
-    n_channels,
-    sfreq=sfreq,
-    n_classes=n_classes,
-    input_size_s=input_size_samples / sfreq
-)
-
-# Export model to device
-print(f'Using device \'{device}\'.')
-model = model.to(device)
+clf = torch.load(models_path)
 
 # %%
-# 5. Training and testing
-
-train_bal_acc = EpochScoring(
-    scoring='balanced_accuracy', on_train=True, name='train_bal_acc',
-    lower_is_better=False)
-valid_bal_acc = EpochScoring(
-    scoring='balanced_accuracy', on_train=False, name='valid_bal_acc',
-    lower_is_better=False)
-callbacks = [('train_bal_acc', train_bal_acc),
-             ('valid_bal_acc', valid_bal_acc)]
-
-clf = EEGClassifier(
-    model,
-    criterion=torch.nn.CrossEntropyLoss,
-    optimizer=torch.optim.Adam,
-    train_split=predefined_split(valid_set),  # using valid_set for validation
-    optimizer__lr=lr,
-    batch_size=batch_size,
-    callbacks=callbacks,
-    device=device
-)
-
-# Model training for a specified number of epochs. `y` is None as it is already
-# supplied in the dataset.
-clf.fit(train_set, y=None, epochs=n_epochs)
+# 5. Testing
 
 y_true = np.concatenate(
     tuple([test_set.datasets[i].windows.metadata['target'].values
